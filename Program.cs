760e306adf533a504a7381878059ab0e723e94f9
Bartoloss <108295549+Bartoloss@users.noreply.github.com>
2022-10-18 14:59:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Guess the number drawn by the computer in the range of 1 to 10!");
+Random random = new Random();
+int a = random.Next(1, 10);
+
+int b;
+int howMany = 0;
+
+Console.Write("Please write the number: ");
+do
+{
+
+    b = int.Parse(Console.ReadLine());
+
+    if (b <= 10 && b >= 1)
+    {
+
+        howMany++;
+        if (b == a)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Congratulations! You guessed the number in {0} total!", howMany);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        else if (b > a)
+        {
+            Console.WriteLine("Unfortunately! The number you entered is greater than the nummber randomly selected");
+            Console.Write("Please write the number again: ");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately! The number you entered is lower than the nummber randomly selected");
+            Console.Write("Please write the number again: ");
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("The number you entered is out of range! Please enter the number again: ");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+} while (a != b);
